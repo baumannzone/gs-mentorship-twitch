d9b180eac5f6ab2342ad11390bf27d0768699e0b
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-05-28 13:10:41 +0200

    feat: initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hola que tal?" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
