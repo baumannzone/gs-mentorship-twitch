cea4f88d68f62e6c84cd07710e97658d4f218c11
aldorea <adomenechreal@gmail.com>
2021-06-16 11:29:06 +0200

    Save access token Co-authored-by: Jorge Baumann <baumannzone@users.noreply.github.com>

--- src/views/Redirect.vue
+++ src/views/Redirect.vue
@@ -1,11 +1,34 @@
 <template>
-  <pre>
-    {{ $route }}
-  </pre>
+  <div>
+    <button @click="f">Save token</button>
+    <button @click="getStoreToken">Show token</button>
+    <h2>{{ tokenFromStore }}</h2>
+    <pre>
+      {{ $route }}
+    </pre>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapMutations } from "vuex";
+export default {
+  created() {
+    // Loading...
+    const fullPath = this.$route.fullPath;
+    const token = fullPath.split("#access_token=")[1].split("&")[0];
+    this.SET_TWITCH_TOKEN(token);
+    this.$router.push({ name: "Overlay" });
+  },
+  data() {
+    return {
+      token: null,
+      tokenFromStore: null,
+    };
+  },
+  methods: {
+    ...mapMutations(["SET_TWITCH_TOKEN"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
